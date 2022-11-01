d0a7b883e891b6d13a053df7df62717514f7dd6a
c0dject <creseeds@gmail.com>
2022-11-01 17:22:54 +0900

    Add: dotenv, cors, morgan setting

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
+const dotenv = require('dotenv');
+dotenv.config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
 
 const routes = require('./routes');
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+
 const app = express();
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
+app.use(morgan('combined'));
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 10010;
