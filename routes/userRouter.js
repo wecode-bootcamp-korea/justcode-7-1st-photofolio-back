ad10a66f4c87ba95360f06f9c602f06cc7e1006d
Jisoo <k3ji0706@gmail.com>
2022-11-04 15:46:40 +0900

    ADD : upload1

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({
+  dset: 'uploads/'
+})
 
 const { validateToken } = require('../middlewares/validateToken');
 const userController = require('../controllers/userController');
@@ -12,5 +16,7 @@ router.post('/login', userController.loginUser);
 router.post('/accountInfo', validateToken, userController.getAccountInfo);
 
 // router.get('/test', userController.layerConnectionTest);
+
+router.post('/profile', upload.single('image'), userController.uploadProfile);
 
 module.exports = router;
