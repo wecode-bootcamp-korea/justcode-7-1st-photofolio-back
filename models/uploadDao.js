536504e4827865395d1501ab3fbe824b5db584b1
Jisoo <k3ji0706@gmail.com>
2022-11-06 19:31:06 +0900

    수정 : 게시물 업로드

--- models/uploadDao.js
+++ models/uploadDao.js
@@ -9,36 +9,55 @@ const myDataSource = new DataSource({
 });
 myDataSource.initialize()
 
-const worksCategory = async category_name => {
-  const category_id = await myDataSource.query(`
+const worksCategory = async (category_name) => {
+  console.log('다오1')
+  const [categoryId] = await myDataSource.query(`
     SELECT id FROM Works_category WHERE category_name = '${category_name}'
   `);
+  const category_id = categoryId.id
   return category_id;
 };
 
-const publicStatus = async statusName => {
-  const status_id = await myDataSource.query(`
-    SELECT id FROM public_status WHERE status = '${statusName}'
+const publicStatus = async (public_status) => {
+  console.log('다오2')
+  const [statusId] = await myDataSource.query(`
+    SELECT id FROM public_status WHERE status = '${public_status}'
   `);
+  const status_id = statusId.id
   return status_id;
 };
 
+const findTilte = async (title, user_id) => {
+  console.log('다오3')
+  const tilteName = await myDataSource.query(`
+  SELECT title FROM Works_Posting WHERE title = '${title}' and user_id = '${user_id}'
+`);
+  return tilteName
+}
+
+
 const uploadForm = async (title, content, user_id, category_id, status_id) => {
+  console.log('다오4')
   const posting = await myDataSource.query(`
   INSERT INTO Works_Posting (user_id, category_id, title, content, status_id)
   VALUES ('${user_id}', '${category_id}', '${title}', '${content}', '${status_id}')
   `)
+  console.log(posting)
   return posting
 }
 
 const worksPosting = async (user_id, title) => {
-  const posting_id = await myDataSource.query(`
+  console.log('다오5')
+  const postingId = await myDataSource.query(`
     SELECT id FROM Works_Posting WHERE user_id = '${user_id}' AND title='${title}'
   `)
+  console.log(postingId)
+  const posting_id = postingId[0].id
   return posting_id
 }
 
 const uploadImages = async (posting_id, path) => {
+  console.log('다오6')
   for (let i = 0; i < path.length; i++) {
     await myDataSource.query(`
   INSERT INTO upload_file (posting_id, file_sort_id, upload_url)
@@ -47,14 +66,8 @@ const uploadImages = async (posting_id, path) => {
   }
 }
 
-const uploadTest = async (path1) => {
-  for (let i = 0; i < path1.length; i++) {
-    await myDataSource.query(`
-  INSERT INTO upload_file (posting_id, file_sort_id, upload_url)
-  VALUES ('1', '1', '${path1[i]}')
-  `)
-  }
-}
 
 
-module.exports = { worksCategory, publicStatus, uploadForm, worksPosting, uploadImages, uploadTest }
+
+
+module.exports = { worksCategory, publicStatus, findTilte, uploadForm, worksPosting, uploadImages }
