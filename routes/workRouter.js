19b84de35a4a53f2d34841e324359624c62b4f7d
c0dject <creseeds@gmail.com>
2022-11-07 23:06:27 +0900

    Modify: 'HTTP method changed to DELETE -  follow cancel

--- routes/workRouter.js
+++ routes/workRouter.js
@@ -9,6 +9,10 @@ const workController = require('../controllers/workController');
 router.get('/list', workController.worksList);
 router.get('/:id', workController.feed); // 토큰여부 필요함
 router.post('/following', validateToken, workController.following); //토큰 필요!!
-router.post('/following-cancel', validateToken, workController.followingCancel); //토큰 필요!!
+router.delete(
+  '/following-cancel',
+  validateToken,
+  workController.followingCancel
+); //토큰 필요!!
 
 module.exports = router;
