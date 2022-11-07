ace22f4d973b3127a137af4e67c98143799af439
Jisoo <k3ji0706@gmail.com>
2022-11-07 18:04:55 +0900

    ADD : 게시물 태그

--- services/uploadService.js
+++ services/uploadService.js
@@ -2,7 +2,6 @@ const uploadDao = require('../models/uploadDao');
 
 
 const uploadImages = async (title, content, arrayTag, image, category_name, user_id, public_status) => {
-  console.log('서비스1')
   const path = image.map(img => img.location);
   if (image === undefined) {
     throw new Error("이미지가 존재하지 않습니다")
@@ -16,34 +15,20 @@ const uploadImages = async (title, content, arrayTag, image, category_name, user
     throw new Error("태그는 10개까지만 할 수 있어요.")
   }
   const category_id = await uploadDao.worksCategory(category_name);
-  console.log('서비스2')
   const status_id = await uploadDao.publicStatus(public_status);
-  console.log('서비스3')
   const tilteName = await uploadDao.findTilte(title, user_id);
-  console.log('서비스4')
+
   if (tilteName.length !==0 ) {
     throw new Error('같은 제목이 이미 존재합니다.');
   }
-  await uploadDao.uploadForm(title, content, user_id, category_id, status_id);
-  console.log('서비스5')
-  const posting_id = await uploadDao.worksPosting(user_id, title);
-  console.log('서비스6')
-  await uploadDao.uploadImages(posting_id, path);
-  console.log('서비스7')
-  await uploadDao.worksTagNames(arrayTag);
-  console.log('서비스8')
-  await uploadDao.deleteOverlapTag();
-  console.log('서비스9')
-  await uploadDao.findTagId(arrayTag);
-  console.log('서비스10')
-}
 
-const uploadTest = async (image) => {
-  const path1 = image.map(img => img.location);
-  if (image === undefined) {
-    return res.status(400).send(util.fail(400, "이미지가 존재하지 않습니다"))
-  }
-  await uploadDao.uploadImages(path1)
+  await uploadDao.uploadForm(title, content, user_id, category_id, status_id);
+  const posting_id = await uploadDao.worksPosting(user_id, title);
+  await uploadDao.uploadImages(posting_id, path);
+  await uploadDao.worksTagNames(arrayTag);
+  await uploadDao.deleteOverlapTag();
+  const tagId = await uploadDao.findTagId(arrayTag);
+  await uploadDao.worksPostingTags(tagId, posting_id)
 }
 
 
