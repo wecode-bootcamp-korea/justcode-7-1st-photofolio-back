d747118dfa31046654f7d38639dc73b613ce71bb
c0dject <creseeds@gmail.com>
2022-11-08 23:32:48 +0900

    Fix: Conflict resolution after merge

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// merge이후 오류로 인한 우선 주석처리
-// const upload = require('../middlewares/multer');
+const upload = require('../middlewares/multer');
 
 const { validateToken } = require('../middlewares/validateToken');
 
-// merge이후 오류로 인한 우선 주석처리
-// const uploadController = require('../controllers/uploadController');
+const uploadController = require('../controllers/uploadController');
 
 //여러장 사진 업로드
 router.post(
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-// const upload = require('../middlewares/multer');
+const upload = require('../middlewares/multer');
 
 const { validateToken } = require('../middlewares/validateToken');
 const userController = require('../controllers/userController');
 
 //회원가입
-// merge이후 오류로 인한 우선 주석처리
-// router.post('/signup', upload.single('profile'), userController.createUser);
+router.post('/signup', upload.single('profile'), userController.createUser);
 
 //로그인
 router.post('/login', userController.loginUser);
 //계정정보조회페이지
 router.post('/accountInfo', validateToken, userController.getAccountInfo);
 
+// 업로드 관련 뭔가가 빠진듯 서버 안돌아가서 주석처리
 // router.get('/test', userController.layerConnectionTest);
 
 module.exports = router;
